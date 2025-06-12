f442b2be4bb21990d53b3f3e15bea433ccece22b
Leumor <116955025+leumor@users.noreply.github.com>
2025-06-12 20:47:11 +0100

    Refactor NodeKeyBlock and subclasses
    This commit refactors the `NodeKeyBlock` hierarchy:
    
    - `NodeKeyBlock` is changed from an abstract class to an interface.
    - `NodeChkBlock` and `NodeSskBlock` are converted to data classes, implementing the `NodeKeyBlock` interface.
    - `equals()` and `hashCode()` methods are implemented for `NodeChkBlock` and `NodeSskBlock` to ensure proper comparison and hashing based on their `data`, `headers`, and `nodeKey` properties.

--- composeApp/src/commonMain/kotlin/network/crypta/entry/keyblock/NodeKeyBlock.kt
+++ composeApp/src/commonMain/kotlin/network/crypta/entry/keyblock/NodeKeyBlock.kt
@@ -4,15 +4,60 @@ import network.crypta.entry.key.NodeChk
 import network.crypta.entry.key.NodeKey
 import network.crypta.entry.key.NodeSsk
 
-abstract class NodeKeyBlock<K : NodeKey>(
-    val data: ByteArray,
-    val headers: ByteArray,
+interface NodeKeyBlock<K : NodeKey> {
+    val data: ByteArray
+    val headers: ByteArray
     val nodeKey: K
-) {
 }
 
-class NodeChkBlock(data: ByteArray, headers: ByteArray, nodeKey: NodeChk) :
-    NodeKeyBlock<NodeChk>(data, headers, nodeKey)
+data class NodeChkBlock(
+    override val data: ByteArray,
+    override val headers: ByteArray,
+    override val nodeKey: NodeChk
+) : NodeKeyBlock<NodeChk> {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
 
-class NodeSskBlock(data: ByteArray, headers: ByteArray, nodeKey: NodeSsk) :
-    NodeKeyBlock<NodeSsk>(data, headers, nodeKey)
+        other as NodeChkBlock
+
+        if (!data.contentEquals(other.data)) return false
+        if (!headers.contentEquals(other.headers)) return false
+        if (nodeKey != other.nodeKey) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = data.contentHashCode()
+        result = 31 * result + headers.contentHashCode()
+        result = 31 * result + nodeKey.hashCode()
+        return result
+    }
+}
+
+data class NodeSskBlock(
+    override val data: ByteArray,
+    override val headers: ByteArray,
+    override val nodeKey: NodeSsk
+) : NodeKeyBlock<NodeSsk> {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as NodeSskBlock
+
+        if (!data.contentEquals(other.data)) return false
+        if (!headers.contentEquals(other.headers)) return false
+        if (nodeKey != other.nodeKey) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = data.contentHashCode()
+        result = 31 * result + headers.contentHashCode()
+        result = 31 * result + nodeKey.hashCode()
+        return result
+    }
+}
